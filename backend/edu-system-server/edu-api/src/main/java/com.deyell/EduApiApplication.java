0babf567d711868f3d4804fe9135292e8de01aa1
duyell <534731500@qq.com>
2026-04-07 19:42:43 +0800

    feat: 创建项目的基础结构

---
+++ backend/edu-system-server/edu-api/src/main/java/com.deyell/EduApiApplication.java
@@ -0,0 +1,15 @@
+package com.deyell;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author duyell
+ */
+@SpringBootApplication
+public class EduApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduApiApplication.class, args);
+    }
+}
